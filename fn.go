9d152fa1cedd8f89c6cf5700192f4c3cc46496c8
Limarev Denis <denislimarev@gmail.com>
2025-10-16 16:17:09 +0500

    init module

---
+++ fn.go
@@ -0,0 +1,51 @@
+// nolint
+package externalsort
+
+import (
+	"bytes"
+	"io"
+
+	"github.com/pkg/errors"
+)
+
+func ReadLine(reader io.ReadSeeker) ([]byte, error) {
+	startPos, err := reader.Seek(0, io.SeekCurrent)
+	var readOffset int64
+	var readBuffer bytes.Buffer
+
+	for {
+		tmp := make([]byte, 256)
+		n, err := reader.Read(tmp)
+		if n > 0 {
+			if pos := bytes.IndexByte(tmp, '\n'); pos > -1 {
+				readOffset += int64(pos) + 1
+				if _, err = readBuffer.Write(append(tmp[:pos], []byte("\n")...)); err != nil {
+					return nil, err
+				}
+				break
+			}
+
+			readOffset += int64(n)
+			if _, err = readBuffer.Write(tmp); err != nil {
+				return nil, err
+			}
+		}
+
+		if err != nil {
+			if err == io.EOF && readBuffer.Len() > 0 {
+				if _, err = readBuffer.Write([]byte("\n")); err != nil {
+					return nil, err
+				}
+				break
+			}
+			return nil, err
+		}
+
+	}
+
+	if _, err = reader.Seek(startPos+readOffset, io.SeekStart); err != nil {
+		return nil, errors.WithMessage(err, "on Seek")
+	}
+
+	return readBuffer.Bytes(), nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/peakle/external-sort
+
+go 1.24.7
+
+require github.com/pkg/errors v0.9.1
